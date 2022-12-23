d7c778d334bfc16b544e63315afb83115ff1146c
danielhmendoza01 <danielhmendoza01@gmail.com>
2022-12-22 22:17:14 -0700

    Fixed ingredient error

--- Testing/copy.ps1
+++ Testing/copy.ps1
@@ -1,13 +1,13 @@
 # Define the source and destination paths
-$file = "error.xlsx"
+$file = "active.xlsx"
 $source = "C:\Users\Daniel\Documents\opiod_list\Atomatic_Code_Sets\NDC\Antidepressants\$file"
-$destination = "C:\Users\Daniel\Dropbox (ASU)\RxNorm Opioids\NDC Paper\NDC Paper\Appendices"
+$destination = "C:\Users\Daniel\Dropbox (ASU)\RxNorm Opioids\NDC\NDC Paper\Appendices"
 
 # Copy the file to the destination folder
 Copy-Item $source $destination
 
 # Define the new name for the file
-$newName = "Appendix15.xlsx"
+$newName = "Appendix14.xlsx"
 
 # Change the name of the copied file
 Rename-Item "$destination\$file" $newName
