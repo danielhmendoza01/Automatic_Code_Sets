c864824580e90742f1f555719eb863e02822f72b
danielhmendoza01 <danielhmendoza01@gmail.com>
2023-05-04 01:55:21 -0700

    output with updated ingredient/No more nonopioid

--- Testing/copy.ps1
+++ Testing/copy.ps1
@@ -1,10 +1,10 @@
 # Define the source paths and destination path
-$source1 = "C:\Users\Daniel\Documents\opiod_list\Atomatic_Code_Sets\NDC\Opioids"
-$source2 = "C:\Users\Daniel\Documents\opiod_list\Atomatic_Code_Sets\NDC\Antidepressants"
-$destination = "C:\Users\Daniel\Dropbox (ASU)\RxNorm Opioids\NDC\NDC Paper\Appendices"
+$source1 = "C:\Users\Daniel\Documents\opiod_list\Atomatic_Code_Sets\RxNorm\Opioid"
+$source2 = "C:\Users\Daniel\Documents\opiod_list\Atomatic_Code_Sets\RxNorm\Antidepressant"
+$destination = "C:\Users\Daniel\Dropbox (ASU)\RxNorm Opioids\RxNorm Paper\Revision 1\Appendices"
 
 # Define the file names
-$fileNames = "ndc_opioids.xlsx", "Same_Opioid_Medications.xlsx", "New_Opioids_Added.xlsx", "Absent_Opioid.xlsx", "NOT_active.xlsx", "active.xlsx", "ndc_antidepressants.xlsx", "Same_Antidepressants_Medications.xlsx", "New_antidepressants_Added.xlsx", "Absent_Antidepressants.xlsx", "NOT_active.xlsx", "active.xlsx", "error.xlsx"
+$fileNames = "New_Complete_Opioids_List.xlsx", "Same_Opioid_Medications.xlsx", "New_Opioids_Added.xlsx", "Obsolete_Not_Found.xlsx", "Active_Not_Found.xlsx", "New_Complete_Antidepressants_List.xlsx", "Same_Antidepressant_Medications.xlsx", "New_Antidepressant_Added.xlsx","Active_Not_Found_Antidepressant.xlsx"
 
 # Use a for loop to iterate over the array of file names
 for ($i = 0; $i -lt $fileNames.Length; $i++) {
@@ -13,7 +13,7 @@ for ($i = 0; $i -lt $fileNames.Length; $i++) {
   $newName = "Appendix" + ($i + 3) + ".xlsx"
 
   # Set the source path based on the index of the file
-  if ($i -lt 6) {
+  if ($i -lt 5) {
     $source = $source1
   }
   else {
